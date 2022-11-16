3cf4d810d9d60f85f18252efeac16cd40cfe382a
albanlefe <46622964+albanlefe@users.noreply.github.com>
2022-11-16 22:08:38 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApplication2udp/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ConsoleApplication2udp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.button_send = new System.Windows.Forms.Button();
+            this.listBox_received = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Location = new System.Drawing.Point(121, 78);
+            this.textBox_message.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_message.Multiline = true;
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(833, 131);
+            this.textBox_message.TabIndex = 0;
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(121, 217);
+            this.button_send.Margin = new System.Windows.Forms.Padding(4);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(109, 25);
+            this.button_send.TabIndex = 1;
+            this.button_send.Text = "Send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // listBox_received
+            // 
+            this.listBox_received.FormattingEnabled = true;
+            this.listBox_received.ItemHeight = 16;
+            this.listBox_received.Location = new System.Drawing.Point(16, 308);
+            this.listBox_received.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox_received.Name = "listBox_received";
+            this.listBox_received.Size = new System.Drawing.Size(939, 196);
+            this.listBox_received.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(16, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mesage";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(16, 288);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Received Message";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(16, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "IP Destination";
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(121, 7);
+            this.textBox_ip.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(255, 22);
+            this.textBox_ip.TabIndex = 6;
+            this.textBox_ip.Text = "192.168.0.177";
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(121, 39);
+            this.textBox_port.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(255, 22);
+            this.textBox_port.TabIndex = 8;
+            this.textBox_port.Text = "8080";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(16, 43);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(972, 519);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox_received);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.textBox_message);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "UDP Client (Host must running first)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.ListBox listBox_received;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
